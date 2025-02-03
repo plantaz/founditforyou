1ef17cc2e6cb127c449458e65e1c4895f203efdc
plantaz <alexandre@umdeia.com>
2025-02-03 13:28:19 +0000

    Removing {} fro import copyplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // Adjust based on your project structure
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new CopyPlugin({
+        new Dotenv(),
+        new webpack.DefinePlugin({
+            'process.env.MY_GOOGLE_API_KEY': JSON.stringify(process.env.MY_GOOGLE_API_KEY)
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html', // Path to your HTML template
+            filename: 'index.html' // Output file name
+        }),
+        new CopyWebpackPlugin({
             patterns: [
                 { from: 'models', to: 'models' } // Copy /models to dist/models
             ]
         })
     ],
-    mode: 'production'
+    mode: process.env.NODE_ENV || 'development'
 };
